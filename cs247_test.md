c453c9b69e8514f3e276bbd752fe68a0d57c5796
Mufeez Amjad <mufeez.amjad@outlook.com>
2020-06-24 15:53:45 -0400

    move to function and add temp files

--- cs247_test.md
+++ cs247_test.md
@@ -10,12 +10,13 @@ a1/
     test2.in
   ...
 ...
-test.sh
+cs247_test.sh
 ```
 
-- The script will look into a1/* and test questions 1 and 2
-  1. It will compile your `.cc` and `.h` files automatically and link it to a executable called `prog`
-  2. It will run `prog` against `a1q*` where `*` is question 1 or 2 and show you a diff, along with a memory leak summary using `valgrind`
+- The script will look into a`i`/* and test questions `j`
+  1. It will either use your makefile (if one exists) or compile your `.cc` and `.h` files with g++ and link it to a executable called `prog`
+  2. It will run `prog` against `aiqj` and show you a diff, along with a memory leak summary using `valgrind`
+  3. If the test fails, you can inspect the output in the temp files created with your test name.
 - Place your tests ending in `.in` anywhere inside the question folder (you can have a subdirectory for tests)
 
 It outputs the diff (if any) or memory errors (if any) as well as indicator of the tests run:
@@ -24,6 +25,4 @@ It outputs the diff (if any) or memory errors (if any) as well as indicator of t
 - `S` indicates a segfault
 - `.` indicates a pass
 
-**Note**: You can optionally pass in the specific question `./test.sh 1` to only run tests for that question. You can also specify the test to run for a specific question `./test.sh 1 test.in`, which will only run test.in against a1q1.
-
-**Additional Note:** You can make the script test a2/, a3/ by modifying the variable `i` in `test.sh`. 
+**Note**: You can optionally pass in the specific question `./test.sh 1` to only run tests for that question. You can also specify the test to run for a specific question `./test.sh 1 test.in`, which will only run test.in against q1.
--- cs247_test.sh
+++ cs247_test.sh
@@ -6,8 +6,61 @@ failed_sample=()
 failed_diff=()
 tests_res=()
 
+function run_test() {
+    test=$1
+    test_name=$2
+		exec=$3
+		sample=$4
+    tmpfile=$(mktemp "$test_name".XXXXXX)
+		tmpfile2=$(mktemp "$test_name"_sample.XXXXXX)
+    ./$exec < $test 2> /dev/null 1>$tmpfile
+		out1_return=$? #139 if seg fault
+		./$sample < $test > $tmpfile2
+		out2_return=$?
+		diff=$(diff $tmpfile $tmpfile2)
+    different=$?
+    valgrind=$(valgrind --leak-check=full --track-origins=yes --error-exitcode=1 $exec < $test 2>&1)
+    is_mem_leak=$?
+    if [ $different != 0 ] || [ $is_mem_leak == 1 ] || [ $out1_return == 139 ]
+		then
+			failed_names+=($test)
+			if [ $out1_return == 139 ]
+			then
+				failed_out+=("segmentation fault")
+				if [ $out2_return == 139 ]
+				then
+					failed_sample+=("segmentation fault")
+				else
+					failed_sample+=("")
+				fi
+				failed_diff+=("")
+				tests_res+=('S')
+			else
+				if [ "$diff" != "" ]
+				then
+					out1=$(cat $tmpfile)
+					out2=$(cat $tmpfile2)
+					failed_out+=("$out1")
+					failed_sample+=("$out2")
+					failed_diff+=("$diff")
+					tests_res+=('F')
+				else
+					failed_out+=("$valgrind")
+					failed_sample+=("")
+					failed_diff+=("")
+					tests_res+=('M')
+				fi
+			fi
+		else
+			tests_res+=('.')
+			rm "$tmpfile"
+			rm "$tmpfile2"
+		fi
+    
+}
+
 i=2 # assignment number
-for j in 0 1 2 # questions - q0,etc.
+for j in 1 2 # questions - q0,etc.
 do
 	if [ -n "$1" ]
 	then
@@ -33,7 +86,6 @@ do
 	q_dir=a$i/q$j
 	if test -n "$(find $q_dir -regex '.*Makefile')"; then
 		(cd $q_dir/ && make clean && make)
-
 	elif test -n "$(find $q_dir -regex '.*.cc')"; then
 		cc_files=$(ls -A $q_dir/*.cc)
 		g++ -o $q_dir/prog -w $cc_files -O0 -g # note: warnings omitted
@@ -43,46 +95,10 @@ do
 
 	for test in $tests
 	do
-		file1=$q_dir/prog
-		file2=$q_dir/a"$i"q"$j"
-		out1=$($file1 < $test)
-		out1_return=$? #139 if seg fault
-		out2=$($file2 < $test)
-		out2_return=$?
-		diff=$(diff <($file1 < $test) <($file2 < $test))
-		valgrind=$(valgrind --leak-check=full --error-exitcode=1 $file1 < $test 2>&1)
-		is_mem_leak=$?
-		if [ "$diff" != "" ] || [ $is_mem_leak == 1 ] || [ $out1_return == 139 ]
-		then
-			failed_names+=($test)
-			if [ $out1_return == 139 ]
-			then
-				failed_out+=("segmentation fault")
-				if [ $out2_return == 139 ]
-				then
-					failed_sample+=("segmentation fault")
-				else
-					failed_sample+=("")
-				fi
-				failed_diff+=("")
-				tests_res+=('S')
-			else
-				if [ "$diff" != "" ]
-				then
-					failed_out+=("$out1")
-					failed_sample+=("$out2")
-					failed_diff+=("$diff")
-					tests_res+=('F')
-				else
-					failed_out+=("$valgrind")
-					failed_sample+=("")
-					failed_diff+=("")
-					tests_res+=('M')
-				fi
-			fi
-		else
-			tests_res+=('.')
-		fi
+		size=${#test}
+		substr_len=$(expr ${size} - 3)
+		test_name="${test:0:$substr_len}"
+		run_test $test $test_name $q_dir/prog $q_dir/a"$i"q$j
 	done
 done
 
